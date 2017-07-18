6570eec6ddf3618773494ca7ee7536e3072ca025
Aravind00Kumar <Aravindkumarmail@gmail.com>
2017-07-18 11:39:06 +0530

    chore(angularjs): added angularjs examples
    added angularjs examples

--- public/ajs/app.js
+++ public/ajs/app.js
@@ -1,6 +1,35 @@
-(function(){
-    angular.module('app',['ui-router'])
+(function () {
+    angular.module('app', ['ui.router'])
+        .config(['$urlRouterProvider', '$stateProvider', function ($urlRouterProvider, $stateProvider) {
 
+            $stateProvider.state({
+                name: 'home',
+                url: '/home',
+                templateUrl: 'app/views/home-tpl.html',
+                controller: 'app.homeController',
+                controllerAs: 'homeCtrl'
+            })
+                .state({
+                    name: 'doughnut',
+                    url: '/doughnut',
+                    templateUrl: 'app/views/doughnut-tpl.html',
+                    controller: 'app.doughnutController',
+                    controllerAs: 'doughnutCtrl'
+                });
+            $urlRouterProvider.otherwise('/home');
+        }])
+        .run(['$transitions', '$rootScope', function ($transitions, $rootScope) {
+            var criteria = {
+                to: function (state) {
+                    return state.name != null;
+                }
+            }
+            $transitions.onBefore(criteria, function (trans) {
+                $rootScope.stateType = trans.to().name == 'home' ? 'master' : 'slave';
+            });
+        }]);
 
-
+    angular.element(function () {
+        angular.bootstrap(document, ['app']);
+    });
 })();
---
+++ public/ajs/app/controllers/home-ctrl.js
@@ -0,0 +1,9 @@
+(function () {
+
+    function HomeController($rootScope) {
+
+
+    }
+    angular.module('app').controller('app.homeController', ['$rootScope', HomeController]);
+
+})();
