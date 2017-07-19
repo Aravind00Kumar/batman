8b14b891321993d95d0579030f3bf6aac2093d8e
Aravind00Kumar <Aravindkumarmail@gmail.com>
2017-07-19 13:28:29 +0530

    refactor(Core, Components): Added css files to the core and components package
    Added css files to the core and components package

---
+++ packages/aio/module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { BatListComponent } from './list';
+
+@NgModule({
+  declarations: [ BatListComponent ]
+})
+
+export class ComponentsModule {
+  constructor(){
+    
+  }
+}
