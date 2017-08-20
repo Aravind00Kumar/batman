4d11511aa9605aaceac331c9b648c9f73f8f1112
Aravind00Kumar <Aravindkumarmail@gmail.com>
2017-08-20 09:00:37 +0530

    feat(Table): Coulmns sorting | Added and removed columns | Added columns resize | Custom cell templa
    Coulmns sorting | Added and removed columns | Added columns resize | Custom cell template

--- packages/components/src/table/table.ts
+++ packages/components/src/table/table.ts
@@ -15,7 +15,9 @@ export interface ITableOptions {
     height?: number;
     data?: Array<any>;
     pageSize?: number;
-    isCacheEnabled?:boolean;
+    isCacheEnabled?: boolean;
+    sortColumn?: any,
+    sortType?: 'ASC' | 'DESC' | undefined,
     autoPage?: boolean;
     columns?: Array<any>;
 }
@@ -32,7 +34,9 @@ export class Table extends BaseComponent<ITableOptions> implements ITableCompone
         data: [],
         autoPage: true,
         isCacheEnabled: true,
-        columns: []
+        columns: [],
+        sortColumn: undefined,
+        sortType: undefined,
     }
 
     private start: number;
@@ -42,7 +46,10 @@ export class Table extends BaseComponent<ITableOptions> implements ITableCompone
     private containerHeight: number;
     private containerScrollTop: number;
     private scrollTop: number;
-    private scrollLeft: number;
+    private scrollLeft: number = 0;
+
+    private resizeColumn: any;
+    private isResizing: boolean;
 
     constructor(element: HTMLElement, options?: ITableOptions) {
         super('Table', element, { ...Table.defaultOptions, ...options });
@@ -58,59 +65,105 @@ export class Table extends BaseComponent<ITableOptions> implements ITableCompone
         this.start = 0;
         this.end = this.options.pageSize;
         this.activeData = this.options.data.slice(this.start, this.end);
-        console.log(this.activeData.length);
         this.projector.append(this.element, this.render.bind(this));
     }
 
     public render(): VNode {
-        return h('div.table.item-full.no-scroll', [
-            h('div.header.parent', { style: `height: ${this.options.height}px;` }, [
-                h('div.row.child', { style: `height: inherit;left:${-this.scrollLeft}px` }, [this.options.columns.map((item, index) => {
-                    return h('div.cell', { 'style': `min-width:${item.width}px` }, [item.text])
-                })])
-            ]),
+        return h('div.table.parent.item-full.no-scroll', {
+            onmouseup: () => {
+                this.isResizing = false;
+            },
+            onmousemove: (event) => {
+                if (this.isResizing) {
+                    event.preventDefault();
+                    let val = this.resizeColumn.width + event.movementX;
+                    if (val >= 50) this.resizeColumn.width = val;
+                }
+            },
+            onmouseleave: () => {
+                this.isResizing = false;
+            }
+        }, [
+                h('div.header.parent', { style: `height: ${this.options.height}px;` }, [
+                    h('div.row.child', { style: `height: inherit;left:${-this.scrollLeft}px` }, [this.options.columns.filter((col) => { return col.isVisible !== false }).map((item, index) => {
+                        return h('div.cell', {
+                            key: item.name, 'style': `overflow:hidden;text-overflow: ellipsis;white-space: nowrap;min-width:${item.width}px; max-width:${item.width}px`
+                        }, [
+                                h('span.child', {
+                                    'style': `top:0;right:-5px; height:100%; width:11px; cursor:col-resize; z-index:2`,
+                                    onmousedown: (event: any) => {
+                                        event.stopImmediatePropagation();
+                                        event.preventDefault();
+                                        this.isResizing = true;
+                                        this.resizeColumn = item;
+                                        return false;
+                                    }
+                                }),
+                                h('span', {
+                                    style:'cursor:pointer',
+                                    onclick: (event) => {
+                                        if (this.isResizing === false) {
+                                            this.options.sortColumn = item;
+                                            if (this.options.sortType === undefined)
+                                                this.options.sortType = 'ASC';
+                                            else if (this.options.sortType === 'ASC')
+                                                this.options.sortType = 'DESC';
+                                            else if (this.options.sortType === 'DESC')
+                                                this.options.sortType = undefined;
+                                            this.sortData();
+                                        }
+                                    },
+                                }, [h('b', {
+                                    classes: {
+                                        'icon-sort-amount-asc': this.options.sortColumn.name === item.name && this.options.sortType == 'ASC',
+                                        'icon-sort-amount-desc': this.options.sortColumn.name === item.name && this.options.sortType == 'DESC'
+                                    }
+                                }), item.text]),
 
-            h('div.parent', {
-                style: `height: ${this.containerHeight}px;`
-            }, [
-                    h('div.child.container',
-                        { style: `height: ${this.containerHeight}px;`, onscroll: this.scrollEvent.bind(this) },
-                        [
-                            h('div.child.data', {
-                                style: `top:${this.containerScrollTop}px;`
-                            }, [
-                                    h('div.body', { style: `height:${this.options.height}px` }, [this.activeData.map((item, index) => {
-                                        return this.itemTemplate(item, index);
-                                    })])
-                                ]),
-                            h('div.ghost', { style: `height:${this.options.data.length * this.options.height}px` }),
-                        ])
-                ])
-        ]);
+                            ])
+                    })])
+                ]),
+
+                h('div.parent', {
+                    style: `height: ${this.containerHeight}px;`
+                }, [
+                        h('div.child.container',
+                            { style: `height: ${this.containerHeight}px;`, onscroll: this.scrollEvent.bind(this) },
+                            [
+                                h('div.child.data', {
+                                    style: `top:${this.containerScrollTop}px;`
+                                }, [
+                                        h('div.body', { style: `height:${this.options.height}px` }, [this.activeData.map((item, index) => {
+                                            return this.itemTemplate(item, index);
+                                        })])
+                                    ]),
+                                h('div.ghost', { style: `height:${this.options.data.length * this.options.height}px` }),
+                            ])
+                    ])
+            ]);
     }
 
     private cellTemplate(column, item) {
         var index = item.id - 1;
-        if(this.options.isCacheEnabled && this.options.data[index].__cache && this.options.data[index].__cache[column.name]) {
+        if (this.options.isCacheEnabled && this.options.data[index].__cache && this.options.data[index].__cache[column.name]) {
             return this.options.data[index].__cache[column.name];
-        } 
+        }
         let template = document.createElement('template');
         template.innerHTML = column.template;
         let hTemplate = this.hParser((template.content && <HTMLElement>template.content.firstElementChild) || <HTMLElement>template.children[0], item);
-       
-        if(this.options.isCacheEnabled){
+
+        if (this.options.isCacheEnabled) {
             this.options.data[index].__cache = this.options.data[index].__cache || {};
-            this.options.data[index].__cache[column.name] = hTemplate; 
+            this.options.data[index].__cache[column.name] = hTemplate;
         }
-        
+
         return hTemplate;
     }
 
     private itemTemplate(item, index) {
-        return h('div.row', { key: item.id }, [this.options.columns.map((col, index) => {
-            return h('div.cell', { 'style': `min-width:${col.width}px` }, h('div.c', [
-                  (col.template ? this.cellTemplate(col, item) : item[col.name])
-                ]
+        return h('div.row', { key: item.id }, [this.options.columns.filter((col) => { return col.isVisible !== false }).map((col, index) => {
+            return h('div.cell', { key: col.name, 'style': `overflow:hidden;text-overflow: ellipsis;white-space: nowrap;min-width:${col.width}px; max-width:${col.width}px` }, h('div.c', [
+                col.template ? this.cellTemplate(col, item) : item[col.name]]
             ))
         })])
     }
@@ -141,6 +194,23 @@ export class Table extends BaseComponent<ITableOptions> implements ITableCompone
 
             this.scrollLeft = event.currentTarget.scrollLeft;
         }
+    }
+    private sortData() {
+        let colName = this.options.sortColumn.name;
+        if (this.options.data[0][colName] === undefined) {
+            console.log('Sort cannot be applied to a custom column');
+            return;
+        }
+        this.options.data.sort((a, b) => {
+            if (this.options.sortType === 'ASC')
+                return (a[colName] > b[colName]) ? 1 : ((b[colName] > a[colName]) ? -1 : 0);
+            else if (this.options.sortType === 'DESC')
+                return (a[colName] < b[colName]) ? 1 : ((b[colName] < a[colName]) ? -1 : 0);
+            else
+                return (a.id > b.id) ? 1 : ((b.id > a.id) ? -1 : 0);
+        });
+        this.options.data.map((item) => { item.__cache = undefined });
+        this.activeData = this.options.data.slice(this.start, this.end);
     }
     public refresh() {
         this.projector.scheduleRender();
